2f580b0412bf314eada3408a3d05afd04bf6fd43
נתנאל עובדיה <novadia@g.jct.ac.il>
2017-11-29 15:34:00 +0200

    עשיתי את סעיף 3, 4, 5, ואני באמצע של 6.

--- dotNetWPF_03_0535_6349/PrinterUserControl.xaml.cs
+++ dotNetWPF_03_0535_6349/PrinterUserControl.xaml.cs
@@ -23,10 +23,89 @@ namespace dotNetWPF_03_0535_6349
     public partial class PrinterUserControl : UserControl
     {
         static int printerId=1;
+        const double MAX_INK = 100, MIN_ADD_INK=10.0, MAX_PRINT_INK=50.0;
+        const int MAX_PAGES = 400, MIN_ADD_PAGES = 40, MAX_PRINT_PAGES = 200;
+
+        protected string printerName;
+        public string PrinterName { get; set; }
+
+        protected double inkCount;
+        public double InkCount { get; set; }
+
+        protected int pageCount;
+        public int PageCount { get; set; }
+
+        static Random rand = new Random();
+
         public PrinterUserControl()
         {
             InitializeComponent();
             printerNameLabel.Content = "Printer " + printerId++;
+        }
+
+        //returns the percent for ink
+        public double getPercent(double current)
+        {
+            return (MAX_PRINT_INK / current) * 100;
+        }
+        //returns the percent for pages
+        public double getPercent(int current)
+        {
+            return (MAX_PRINT_PAGES / current) * 100;
+        }
+
+        public void printing()
+        {
+            //tells me how much pages and ink i will use now in this printing.
+            double currentPrintIntRequest   = rand.Next((int)MIN_ADD_INK, (int)MAX_PRINT_INK);
+            int currentPrintPageRequest = rand.Next(MIN_ADD_PAGES, MAX_PRINT_PAGES);
+
+
+            //there is enoght ink?
+            if ((inkCount - currentPrintIntRequest)>=0)
+            {
+                inkCount -= currentPrintIntRequest;
+            }
+            else //i don't have enoght ink
+            {
+                //send event.
+                addRandomInk();
+            }
+
+            double currentPercentInk = getPercent(inkCount);
+            //double currentPercentPage = getPercent(pageCount);
+
+            if (currentPercentInk <0.15 && currentPercentInk >=0.10)
+            {
+                printerNameLabel.Foreground = Brushes.Yellow;
+                //send event
+            }
+            else if (currentPercentInk < 0.10 && currentPercentInk >= 0.01)
+            {
+                printerNameLabel.Foreground = Brushes.Orange;
+                //send event
+            }
+            else if (currentPercentInk < 0.01)
+            {
+                printerNameLabel.Foreground = Brushes.Red;
+                //send crucial event
+            }
+
+            if (pageCount == 0)
+            {
+                printerNameLabel.Foreground = Brushes.Red;
+                //send crucial event
+            }
+        }
+
+        public void addRandomInk() //this func adds random amount of ink.
+        {
+            inkCount += rand.Next((int)MIN_ADD_INK, (int)MAX_PRINT_INK);
+        }
+        
+        public void addRadomPage() //this func adds random amount of pages.
+        {
+            pageCount += rand.Next(MIN_ADD_PAGES, MAX_PRINT_PAGES);
         }
     }
 }
